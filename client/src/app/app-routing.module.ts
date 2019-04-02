9e8b33b87868054bd486260eadba9e43f46ae165
Ferenczi Balázs <ferenczi.balazs@outlook.com>
2019-04-02 12:57:18 +0200

    update_3
    update_3

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {PilotListComponent} from './pilot-list/pilot-list.component';
+import {MainMenuComponent} from './main-menu/main-menu.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/pilot-list', pathMatch: 'full' },
+  { path: '', redirectTo: '/main-menu', pathMatch: 'full' },
   {
     path: 'pilot-list',
     component: PilotListComponent
+  },
+  {
+    path: 'main-menu',
+    component: MainMenuComponent
   }
 ];
 
